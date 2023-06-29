43b98d51df499d1723ba2b24809024ccb7ce3661
dr-scooby <hinurali@hotmail.com>
2023-06-29 14:21:08 -0700

    adding DBModel and dao java files

---
+++ JJira/src/main/java/com/DB/DBModel.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.DB;
+
+/**
+ * @author Dr.Scooby
+ *
+ */
+public class DBModel {
+	
+	private String dbname;
+	
+	public DBModel() {
+		
+		dbname = "";
+		
+	}
+
+	
+	
+	public String getDBName() {
+		return dbname;
+	}
+}
