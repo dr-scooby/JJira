f2fc0c673d0099e4ea0316d5e9aa59b1a26f8a46
dr-scooby <hinurali@hotmail.com>
2023-06-29 15:21:01 -0700

    modified ControllerServet. Servlet is working, can reach via Tomcat webserver.

--- JJira/src/main/java/com/web/ControllerServlet.java
+++ JJira/src/main/java/com/web/ControllerServlet.java
@@ -4,6 +4,7 @@
 package com.web;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -40,6 +41,30 @@ public class ControllerServlet extends HttpServlet{
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		
+		// Write the response message, in an HTML page
+		// this block is to test the Servlet & web.xml is working and configure properly.
+	      try {
+	         out.println("<!DOCTYPE html>");
+	         out.println("<html><head>");
+	         out.println("<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>");
+	         out.println("<title>Hello, World</title></head>");
+	         out.println("<body>");
+	         out.println("<h1>Hello, world! You have successfully reached this Servlet..It's working</h1>");  // says Hello
+	         // Echo client's request information
+	         out.println("<p>Request URI: " + request.getRequestURI() + "</p>");
+	         out.println("<p>Protocol: " + request.getProtocol() + "</p>");
+	         out.println("<p>PathInfo: " + request.getPathInfo() + "</p>");
+	         out.println("<p>Remote Address: " + request.getRemoteAddr() + "</p>");
+	         // Generate a random number upon each request
+	         out.println("<p>A Random Number: <strong>" + Math.random() + "</strong></p>");
+	         out.println("</body>");
+	         out.println("</html>");
+	      } finally {
+	         out.close();  // Always close the output writer
+	      }
 	}
 	
 	/**
