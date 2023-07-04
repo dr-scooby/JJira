0cb970a12929cd6622b2aa91d2ceb78fe3f80fee
dr-scooby <hinurali@hotmail.com>
2023-07-04 15:32:06 -0700

    adding modified BugDAO

--- JJira/src/main/java/com/dao/BugDAO.java
+++ JJira/src/main/java/com/dao/BugDAO.java
@@ -6,11 +6,15 @@ package com.dao;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+import com.data.Bug;
+
 /**
  * @author nurali
  *
  */
 public class BugDAO extends DAO{
+	
+	protected Bug abug;
 	
 	protected String createTable = "create table Bugs(id int primary key auto_increment, name varchar(50), description varchar(50), severity int, state varchar(15))";
 	
